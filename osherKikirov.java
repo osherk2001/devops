5229c617c10d2ce9c221aa610e2512108fbeccff
osherk2001 <osherkikirov@gmail.com>
2024-12-14 18:28:43 +0200

    kiki

--- osherKikirov.java
+++ osherKikirov.java
@@ -1,7 +1,7 @@
 public class osherKikirov {
 	public static void main(String[] args){
 		for(int i=1 ; i<10; i++){
-			System.out.println("Osher Kikirov " +i);
+			System.out.println("Osher Kiki " +i);
 		}
 	}
 }	
